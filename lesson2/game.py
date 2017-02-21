29b614fcd5d1f7a9e1cc9128311d9bdb53fb819a
g63336180 <g63336180@gmail.com>
2017-02-21 22:15:50 +0800

    lesson 2 test

---
+++ lesson2/game.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+print("********** Python学习第二课 ****************")
+temp = input("猜一下我心里在想哪个数字：")
+guess = int(temp)
+if guess == 8:
+	print("猜对啦！@！！！！！！！！！！")
+	print("猜中也没有奖")
+else:
+	print("猜错啦，我想的是8")
+print("游戏结束")
