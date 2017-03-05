34c04fbf595abbe154f608f13af86a68c02be9fd
g63336180 <g63336180@gmail.com>
2017-03-05 11:27:24 +0800

    in test

---
+++ lesson5/in.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+database = [
+    ['albert', '1234'],
+    ['dilbert', '4242'],
+    ['smith', '7524'],
+    ['jones', '9843']
+]
+
+username = input("User name: ")
+pin = input("PIN code: ")
+
+if [username, pin] in database:
+    print("Access granted")
+else:
+    print("no permissions")
